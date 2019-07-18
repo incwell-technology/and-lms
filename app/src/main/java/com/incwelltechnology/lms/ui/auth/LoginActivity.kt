2070e9ae9a6d3f59991ce01de024c85c3d4aeace
Rajesh <rajesh.k.khadka@gmail.com>
2019-07-18 16:03:20 +0545

    add base activity

--- app/src/main/java/com/incwelltechnology/lms/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/incwelltechnology/lms/ui/auth/LoginActivity.kt
@@ -1,44 +1,13 @@
 package com.incwelltechnology.lms.ui.auth
 
-import android.os.Bundle
-import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.incwelltechnology.lms.R
+import com.incwelltechnology.lms.base.BaseActivity
 import com.incwelltechnology.lms.databinding.ActivityLoginBinding
-import com.incwelltechnology.lms.util.hide
-import com.incwelltechnology.lms.util.show
-import com.incwelltechnology.lms.util.toast
-import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity(), AuthListener {
+
+class LoginActivity : BaseActivity<ActivityLoginBinding>() {
     val TAG = LoginActivity::class.java.simpleName
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val binding: ActivityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
-        val viewModel = ViewModelProviders.of(this).get(AuthViewModel::class.java)
-        binding.viewmodel = viewModel
-        viewModel.authListener = this
-    }
-
-    override fun onStarted() {
-        progress_bar.show()
-    }
-
-    override fun onSuccess(loginResponse: LiveData<String>) {
-        loginResponse.observe(this, Observer {
-            progress_bar.hide()
-            toast(it)
-            Log.d(TAG, it)
-        })
-    }
-
-    override fun onFailure(message: String) {
-        progress_bar.show()
-        toast(message)
+    override fun getLayout(): Int {
+        return R.layout.activity_login
     }
 }
