49963cda7134e7846b4cfb4e6dbe481d4512aa4d
iamsannyrai <iam.sannyrai@gmail.com>
2019-08-01 09:16:14 +0545

    changes on design

---
+++ app/src/main/java/com/incwelltechnology/lms/DesignActivity.kt
@@ -0,0 +1,12 @@
+package com.incwelltechnology.lms
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DesignActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_design)
+    }
+}
